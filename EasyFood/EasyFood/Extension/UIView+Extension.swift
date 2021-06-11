543cbf56aa4e25592f043eee5321c583ad1e9633
Naseem Oyebola <naseemoyebola@gmail.com>
2021-06-11 12:59:47 +0100

    feat: Interface and File set up

---
+++ EasyFood/EasyFood/Extension/UIView+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIView+Extension.swift
+//  EasyFood
+//
+//  Created by apple on 11/06/2021.
+//
+
+import UIKit
+
+extension UIView {
+   @IBInspectable  var cornerRadius: CGFloat {
+        get {return cornerRadius}
+        set {self.layer.cornerRadius = newValue}
+    }
+}
