2c35abec8f09ec739c40b67145c28e059b42a2ff
Naseem Oyebola <naseemoyebola@gmail.com>
2021-06-21 02:07:04 +0100

    feat: Kingfisher dependency manager feat: category view set up

---
+++ EasyFood/EasyFood/CustomViews/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryCollectionViewCell.swift
+//  EasyFood
+//
+//  Created by apple on 21/06/2021.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryTitleLabel: UILabel!
+    
+    func setUp(category: DishCategory){
+        
+    }
+    
+}
---
+++ EasyFood/EasyFood/Model/DishCategory.swift
@@ -0,0 +1,12 @@
+//
+//  DishCategory.swift
+//  EasyFood
+//
+//  Created by apple on 21/06/2021.
+//
+
+import Foundation
+
+struct DishCategory {
+    let id, name, image: String
+}
